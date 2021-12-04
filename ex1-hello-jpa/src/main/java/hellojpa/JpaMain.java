79de02e1b221b0ac548d7fd607f63a33dc573b10
Hyeon9mak <jinha3507@gmail.com>
2021-12-04 12:29:03 +0900

    feat: 데이터베이스에 엔티티 삽입

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,33 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        // persistence.xml의 <persistence-unit name="hello"> 값을 넘기면 됨
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("구마막");
+            em.persist(member);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            // 내부에서 DB 컬렉션을 물고 동작하기 때문에, 작업이 끝나면 무조건 닫아주어야 한다.
+            em.close();
+        }
+
+        emf.close();
+    }
+}
